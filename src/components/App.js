844c466461785e1f4b47cd007069765ef48311fd
Mertakbas <mertakbas@saatteknoloji.com>
2022-06-12 18:21:54 +0300

    async yöntem

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,25 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos',{
-            params: {query: term},
-            headers: {
-               Authorization: 'Client-ID KSy6eM4iRbWqvgXnHQYZ3mPBrRhK5NH-CFdlsmEDNYs'
-            }
-        })
-        .then(response=>{
-            console.log(response.data.results);
-        });
+    state = { images: [] };
+
+
+    async onSearchSubmit(term) {
+        const response = await axios
+            .get('https://api.unsplash.com/search/photos', {
+                params: { query: term },
+                headers: {
+                    Authorization: 'Client-ID KSy6eM4iRbWqvgXnHQYZ3mPBrRhK5NH-CFdlsmEDNYs'
+                }
+            });
+
+        this.setState({ images: response.data.result });
     }
     render() {
         return (
             <div className='ui container' style={{ marginTop: '10px' }}>
-                <SearchBar onSubmit={this.onSearchSubmit}/>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                Found : {this.state.images.length} images
             </div>
         );
     }
